7a72bb205a3ba6a3971c19565718566eb4f4bd89
whiteedev <160270274+whiteedev@users.noreply.github.com>
2024-04-19 15:40:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 let click = document.querySelector('#clicks')
+var clicks = localStorage.getItem('clicks') || 0;
 
 function incrementClick() {
     click.innerHTML = parseFloat(click.innerHTML) + 1
-}
+} 
+    document.addEventListener("DOMContentLoaded", function(){ document.getElementById("clicks").innerHTML = localStorage.getItem('clicks') || 0; });
+    function onClick() {
+      clicks = +clicks + 1;
+      document.getElementById("clicks").innerHTML = clicks;
+      updateStorage(clicks);
+    };
+
+    function onReset() {
+      if (confirm('Вы уверенны? Данные обнулятся!')) {
+        clicks = 0;
+        document.getElementById("clicks").innerHTML = clicks;
+        updateStorage(clicks);
+      }
+    }
+
+    function updateStorage(clicks) {
+      localStorage.setItem('clicks', clicks);
+    }
+    </script>
